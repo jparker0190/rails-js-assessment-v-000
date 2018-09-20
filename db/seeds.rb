c9463b6b2cb60b3996dbda03fddb67842f3f25d3
Joseph Parker <jparker010190@gmail.com>
2018-09-20 22:55:20 +0000

    getting rid of bad code

--- db/seeds.rb
+++
@@ -1,3 +0,0 @@
-Stock.create(symbol: "FB", sector:"Technology", high: 205, low: 130, price: 170)
-Stock.create(symbol: "TWTR", sector:"Technology", high: 75, low: 13,price: 34)
-Stock.create(symbol: "SNAP", sector:"Technology", high: 34, low: 9, price: 9)
