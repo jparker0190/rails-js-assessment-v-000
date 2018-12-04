b5662c6f4c68a43c4846134bcdbae868a3105afd
Joseph Parker <jparker0190@gmail.com>
2018-12-03 21:09:24 -0500

    adding validations for not allowing the same stock into the portfolio

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,6 +1,7 @@
 class Assignment < ApplicationRecord
     validates :direction, presence: true
     validates :shares, presence: true
+    validates_uniqueness_of :stock_id, :case_sensitive => false
     belongs_to :stock
     belongs_to :portfolio
     def self.alphabatize
