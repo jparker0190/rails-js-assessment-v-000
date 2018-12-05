0f04fdd08b921eb60909885e72a4af9035ed1d76
Joseph Parker <jparker0190@gmail.com>
2018-12-04 22:56:02 -0500

    removing validations for ticker uniqueness

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,7 +1,6 @@
 class Assignment < ApplicationRecord
     validates :direction, presence: true
     validates :shares, presence: true
-    validates_uniqueness_of :stock_id, :case_sensitive => false
     belongs_to :stock
     belongs_to :portfolio
     def self.alphabatize
