f1f3f940cb57871f6902edb279e1563d10515753
Joseph Parker <jparker0190@gmail.com>
2018-12-04 20:26:58 -0500

    updating the pages controller to have assignments root path

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     if user_signed_in?
-      redirect_to portfolios_path
+      redirect_to assignments_path
     else
       redirect_to new_user_session_path
   end
