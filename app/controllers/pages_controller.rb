6e6d813e4b8d5689b6bf5ce7e7256c3c1d3a5232
Joseph Parker <jparker0190@gmail.com>
2018-12-04 20:28:01 -0500

    removing the assignments root path

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     if user_signed_in?
-      redirect_to assignments_path
+      redirect_to portfolios_path
     else
       redirect_to new_user_session_path
   end
