2fcdf11b34868739cfff72021e503672a17d5f67
Joseph Parker <jparker0190@gmail.com>
2018-12-18 20:04:10 -0500

    breaking out js code from erb and adding table form page

--- app/assets/javascripts/portfolios.js
+++ app/assets/javascripts/portfolios.js
@@ -1,24 +1,96 @@
-/*$(function () {
-    $('form').submit(function(event) {
-      //prevent form from submitting the default way
-      event.preventDefault();
- 
-      var values = $(this).serialize();
- 
-      var posting = $.post('/portfolios', values);
- 
-      posting.done(function(data) {
-        var post = data;
-        $("#Portfolioname").text(post["name"]+" is now one of your portfolios ");
-        
-      });
-    });
+fetch("https://api.iextrading.com/1.0/stock/market/list/mostactive")
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(myJson) {
+   for (var key in myJson){
+    console.log(myJson)
+    var percent = myJson[key].changePercent * 100
+    var convert = percent.toFixed(2) + '<br>'
+    var symbol = myJson[key].symbol+'<br>';
+    var price = myJson[key].iexRealtimePrice + '<br>'
+    $("#mostactiveticker").append(symbol);
+    $("#mostactiveprice").append(price);
+    $("#mostactiveper").append(convert);
+   }
   });
-  var theURL = "https://api.iextrading.com/1.0/tops?symbols=SNAP,fb,AIG%2b";
-  $(document).ready(function() {
-   fetch(url)
-   .then(response => response.json())
-   .then(json =>
-    document.getElementById("symbol").innerHTML = `<td>${json[0]['symbol']}</td>`
-    )
-  })*/
+  fetch("https://api.iextrading.com/1.0/stock/market/list/gainers")
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(myJson) {
+   for (var key in myJson){
+    console.log(myJson)
+    var percent = myJson[key].changePercent * 100
+    var convert = percent.toFixed(2) + '<br>'
+    var symbol = myJson[key].symbol+'<br>';
+    var price = myJson[key].iexRealtimePrice + '<br>'
+    $("#gainerticker").append(symbol);
+    $("#gainerprice").append(price);
+    $("#gainerper").append(convert);
+   }
+  });
+  fetch("https://api.iextrading.com/1.0/stock/market/list/losers")
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(myJson) {
+   for (var key in myJson){
+    console.log(myJson)
+    var percent = myJson[key].changePercent * 100
+    var convert = percent.toFixed(2) + '<br>'
+    var symbol = myJson[key].symbol+'<br>';
+    var price = myJson[key].iexRealtimePrice + '<br>'
+    $("#loserticker").append(symbol);
+    $("#loserprice").append(price);
+    $("#loserper").append(convert);
+   }
+  });
+  fetch("https://api.iextrading.com/1.0/stock/market/list/iexvolume")
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(myJson) {
+   for (var key in myJson){
+    console.log(myJson)
+    var percent = myJson[key].changePercent * 100
+    var convert = percent.toFixed(2) + '<br>'
+    var symbol = myJson[key].symbol+'<br>';
+    var price = myJson[key].iexRealtimePrice + '<br>'
+    $("#volumeticker").append(symbol);
+    $("#volumeprice").append(price);
+    $("#volumeper").append(convert);
+   }
+  });
+  fetch("https://api.iextrading.com/1.0/stock/market/list/iexpercent")
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(myJson) {
+   for (var key in myJson){
+    console.log(myJson)
+    var percent = myJson[key].changePercent * 100
+    var convert = percent.toFixed(2) + '<br>'
+    var symbol = myJson[key].symbol+'<br>';
+    var price = myJson[key].iexRealtimePrice + '<br>'
+    $("#percentticker").append(symbol);
+    $("#percentprice").append(price);
+    $("#percentper").append(convert);
+   }
+  });
+  fetch("https://api.iextrading.com/1.0/stock/market/list/infocus")
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(myJson) {
+   for (var key in myJson){
+    console.log(myJson)
+    var percent = myJson[key].changePercent * 100
+    var convert = percent.toFixed(2) + '<br>'
+    var symbol = myJson[key].symbol+'<br>';
+    var price = myJson[key].iexRealtimePrice + '<br>'
+    $("#focusticker").append(symbol);
+    $("#focusprice").append(price);
+    $("#focusper").append(convert);
+   }
+  });
